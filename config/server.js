33012789398bd813cf360fd48a562dd4eff8dce7
yousufnzm <yousufnzm@gmail.com>
2021-12-07 23:49:10 +0530

    strapi learn

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '28eb62aac32114bd324c53cafc7a77ed'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3b783953-294c-4146-8150-94a41edf944a'
+};
